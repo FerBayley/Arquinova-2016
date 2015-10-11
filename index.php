92e2eae4c054789253c2a35e17d55b6352ecb80b
fernando.bayley@gmail.com <fernando.bayley@gmail.com>
2015-10-11 19:57:42 -0300

    Primer commit - archivos base

---
+++ index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Arquinova Color - Revestimientos y pinturas</title>
+	<meta name="description" content="Trabajamos con las mejores marcas en pinturas y revestimeintos para que su proyecto arquitectonico se realice con la mejor calidad">
+	<!-- VIEWPORT -->
+	<meta name="viewport" content="width=device-width, user-scalable=no"/>
+	<!-- VIEWPORT -->
+	<!-- CSS -->
+	<link rel="stylesheet" href="css/normalize.css" />
+	<link rel="stylesheet" href="css/main.css" />
+	<!-- CSS -->
+	<!-- JS -->
+	<script src="js/modernizr.custom.59319.js"></script>
+	<script src="js/prefixfree.min.js"></script>
+	<!-- JS -->
+	<!-- FAVICON -->
+	<link rel="shortcut icon" href="favicon.ico" />
+	<!-- FAVICON -->
+	<!--[if lt IE 9]>
+    	<script src="components/html5shiv/html5shiv.js"></script>
+	<![endif]-->
+</head>
+<body>	
+	<section class="Contenedor__central">
+
+		<div class="Logous">
+			<img src="img/logo.jpg" width= "150" height="110" alt="Arquinova Color">
+		</div>
+
+		<div class="Container-cont">
+			<h1>Arquinova Color</h1>
+			<h2>Estamos trabajando en un nuevo Arquinova Color, pronto estaremos de vuelta.</h2>
+			<p>Beauchef 1823 Capital Federal - 4922-2926 - Nextel 694*4170 - lun a vier de 8 a 18hs y sab 8:30 a 12:30hs</p>
+			<p>Mariano Castex 2735 - 4-295-7501 - Canning - Ezeiza - lun a vier de 9 a 18hs y sab 9 a 17hs</p>
+			<p>Todos los derechos reservados &copy;2014 / <a href="mailto:info@arquinovacolor.com.ar">info@arquinovacolor.com.ar</a></p>
+		</div> <!-- End of Container-cont -->
+
+	</section> <!-- End of Contenedor__central -->	
+
+	<?php include("includes/metricas.php"); ?>
+	<?php include("includes/olark.php"); ?>
+
+</body>
+</html>
